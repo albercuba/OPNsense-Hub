17494e002b78beea672aa64d28f1be91896f03df
albercuba <albercuba@gmail.com>
2026-06-21 18:50:45 +0200

    chown -R root:wheel /root/.ssh

--- net-mgmt/os-opnsensehub/src/opnsense/mvc/app/controllers/OPNsense/OPNsenseHub/IndexController.php
+++ net-mgmt/os-opnsensehub/src/opnsense/mvc/app/controllers/OPNsense/OPNsenseHub/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseIndexController
 {
     public function indexAction()
     {
-        $this->view->pick('OPNsenseHub/index');
+        $this->view->pick('OPNsense/OPNsenseHub/index');
         $this->view->formGeneral = $this->getForm('general');
     }
 }
